a63db287189e8dbfb4f8447e4e01f5b33b4b60b1
aitinsan <57362967+aitinsan@users.noreply.github.com>
2020-12-25 22:03:02 +0600

    Добавьте файлы проекта.

---
+++ EmailService/EmailConfigurationClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmailService
+{
+    public class EmailConfigurationClass
+    {
+        public string From { get; set; }
+        public string Smtp { get; set; }
+        public int Port { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ SomeNewWarker/Worker.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using EmailService;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace SomeNewWarker
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly IEmailSender _sender;
+        private FileSystemWatcher watcher;
+        private readonly string path = @"C:\Users\Insan\Desktop\MyFolder";
+
+        public Worker(ILogger<Worker> logger, IEmailSender sender)
+        {
+            _logger = logger;
+            _sender = sender;
+            
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            watcher = new FileSystemWatcher();
+            watcher.Path = path;
+            watcher.Created += OnChanged;
+            return base.StartAsync(cancellationToken);
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            _logger.LogInformation("New message is about to send at: {time}", DateTimeOffset.Now);
+            var message = new Message(new string[] { "newsomeone229@gmail.com" }, "MY Test Subject","Hello From Insan", e.FullPath);
+            _sender.SendEmail(message);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path); 
+
+                }
+                watcher.EnableRaisingEvents = true;
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(5000, stoppingToken);
+            }
+        }
+    }
+}
